4246e285b78902a81b2c544092bd7d10ae145715
qa-bot287 <fa.so2017@yandex.ru>
2022-06-10 10:15:50 +0500

    game v1

--- main.py
+++ main.py
@@ -1,82 +1,21 @@
-import turtle
-from math import pi, sin, cos
+import pygame
+from gun import Gun
+import controls
+from pygame.sprite import Group
 
 
-# Нарисовать букву S (Упражнение 2)
-def turtle_s_draw():
-    turtle.shape('turtle')
-    turtle.delay(150)
-    turtle.forward(50)
-    turtle.left(90)
-    turtle.forward(50)
-    turtle.left(90)
-    turtle.forward(50)
-    turtle.right(90)
-    turtle.forward(50)
-    turtle.right(90)
-    turtle.forward(50)
+def run():
+    pygame.init()
+    screen = pygame.display.set_mode((700, 800))
+    pygame.display.set_caption("Cosmo defenders")
+    bg_color = (0, 0, 0)
+    gun = Gun(screen)
+    bullets = Group()
+    while True:
+        controls.events(screen, gun, bullets)
+        gun.update_gun()
+        controls.update(bg_color, screen, gun, bullets)
+        controls.update_bullets(bullets)
 
 
-# Нарисовать квадрат (Упражнение 3)
-def turtle_square():
-    turtle.shape('turtle')
-    turtle.delay(150)
-    turtle.forward(100)
-    turtle.left(90)
-    turtle.forward(100)
-    turtle.left(90)
-    turtle.forward(100)
-    turtle.left(90)
-    turtle.forward(100)
-
-
-# Нарисовать окружность (Упражнение 4)
-def turtle_circle():
-    turtle.shape('turtle')
-    turtle.circle(100)
-
-
-# Нарисовать вложенные квадраты (Упражнение 5)
-def turtle_inner_square():
-    turtle.shape('turtle')
-    r = 20
-    turtle.left(45)
-    for i in range(10):
-        turtle.circle(r, 360, 4)
-        turtle.penup()
-        turtle.goto(r - r / 4, -r + r / 4)
-        turtle.pendown()
-        r = r + 20
-
-
-# Нарисовать паука, где n = 12 (Упражнение 6)
-def turtle_spider():
-    turtle.shape("turtle")
-    turtle.goto(0, 0)
-
-    n = 12
-
-    for i in range(n):
-        turtle.forward(150)
-        turtle.stamp()
-        turtle.left(360)
-        turtle.backward(150)
-        turtle.left(360)
-        turtle.right(360 / n)
-
-
-def spiral():
-    turtle.shape('turtle')
-    turtle.delay(50)
-    for i in range(200):
-        t = i / 10 * pi
-        dx = t * cos(t)
-        dy = t * sin(t)
-        turtle.goto(dx, dy)
-
-# turtle_square()
-# turtle_s_draw()
-# turtle_circle()
-# turtle_inner_square()
-# turtle_spider()
-# spiral()
+run()
