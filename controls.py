b41018c870379194c1eba77349a7fb101df03af0
qa-bot287 <fa.so2017@yandex.ru>
2022-06-10 10:11:00 +0500

    game v1

--- controls.py
+++ controls.py
@@ -1,8 +1,9 @@
 import pygame
 import sys
+from bullet import Bullet
 
 
-def events(gun):
+def events(screen, gun, bullets):
     """event handling"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -13,6 +14,9 @@ def events(gun):
                 gun.move_right = True
             elif event.key == pygame.K_LEFT:
                 gun.move_left = True
+            elif event.key == pygame.K_SPACE:
+                new_bullet = Bullet(screen, gun)
+                bullets.add(new_bullet)
         elif event.type == pygame.KEYUP:
             # Right
             if event.key == pygame.K_RIGHT:
@@ -21,8 +25,18 @@ def events(gun):
                 gun.move_left = False
 
 
-def update(bg_color, screen, gun):
+def update(bg_color, screen, gun, bullets):
     """Update screen"""
     screen.fill(bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     gun.output()
     pygame.display.flip()
+
+
+def update_bullets(bullets):
+    """Update bullets position"""
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
--- game_echo.py
+++ game_echo.py
@@ -1,6 +1,7 @@
 import pygame
 from gun import Gun
 import controls
+from pygame.sprite import Group
 
 
 def run():
@@ -9,11 +10,12 @@ def run():
     pygame.display.set_caption("Cosmo defenders")
     bg_color = (0, 0, 0)
     gun = Gun(screen)
-
+    bullets = Group()
     while True:
-        controls.events(gun)
+        controls.events(screen, gun, bullets)
         gun.update_gun()
-        controls.update(bg_color, screen, gun)
+        controls.update(bg_color, screen, gun, bullets)
+        controls.update_bullets(bullets)
 
 
 run()
